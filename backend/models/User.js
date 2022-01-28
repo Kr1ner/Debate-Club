f0774279e84974419f279f11d6880a2f3716a553
Andron <roniosipov7@gmail.com>
2022-01-28 20:45:02 +0400

    Update #2

--- backend/models/User.js
+++ backend/models/User.js
@@ -4,14 +4,18 @@ const Schema = mongoose.Schema
 const userSchema = new Schema({
     name: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     }
-} , {timestamps: true} )
+} , {timestamps: true})
+
+module.exports = mongoose.model("User", userSchema);
