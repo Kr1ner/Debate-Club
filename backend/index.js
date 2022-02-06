e2f263b247052042bb337352bba8e5801830f319
Kriner <krineri12@gmail.com>
2022-02-06 13:01:57 +0400

    react js package modules and html script

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,11 @@ const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const authRoute = require("./routes/auth0");
 const userRoute = require("./routes/users");
+import React from 'react';
+import React from 'react-dom';
+import Page from '../frontend/Page';
+
+ReactDOM.render(<Page />,document.getElementById())
 
 dotenv.config();
 app.use(express.json());
---
+++ frontend/Page.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import React from 'react-dom';
+
+function header(){
+    return (
+        <div>
+            <h1>Depsacito Epicuri</h1>
+        </div>
+    )
+}
+
+function Page(){
+    return(
+        <div>
+            <header />
+        </div>
+    )
+}
+
+export default Page;
